993e60392c27cbcfdef2e5c80138f7a1594fa843
Eduardo Finotti <eduardo.finotti@jamesdelivery.com.br>
2021-08-23 16:17:43 -0300

    first commit

---
+++ codecept.conf.js
@@ -0,0 +1,41 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://automationpractice.com/index.php',
+      browser: 'chrome',
+      show: true,
+
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  teardown: null,
+  mocha: {},
+  name: 'curso-codeceptjs-matiana',
+  plugins: {
+    wdio: {
+      enabled: true,
+      services: ['selenium-standalone']
+    },
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
---
+++ upload_test.js
@@ -0,0 +1,16 @@
+Feature('Upload File');
+
+Scenario('Fazer upload de imagem', ({ I }) => {
+    I.amOnPage('?controller=contact')
+
+    // I.selectOption('#id_contact', "1")
+    // I.fillField('#email', 'finotti@qazando.com')
+    // I.fillField('#id_order', '123456')
+    // I.fillField('#message', 'Minha mensagem')
+
+    // pause()
+    // I.attachFile('#fileUpload', 'images/image.png')
+
+    // I.click('#submitMessage')
+
+});
